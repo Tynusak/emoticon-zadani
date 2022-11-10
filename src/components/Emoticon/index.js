69bdec91fa8b2249c7e3abc8cfaf6e4b77c1045f
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-11-10 12:06:55 +0100

    -

--- src/components/Emoticon/index.js
+++ src/components/Emoticon/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './style.css';
-
-import eyesImg from '../../img/eyes2.svg';
-import mouthImg from '../../img/mouth2.svg';
+import { useSettings } from '../../setting-context';
+import { eyesData, mouthData, colorsData } from '../../data';
 
 const Emoticon = () => {
+  const settings = useSettings();
 
-	return (
-		<div className='emoticon' style={{ backgroundColor: '#ff2e12' }}>
-			<img className="emoticon__eyes" src={eyesImg} />
-			<img className="emoticon__mouth" src={mouthImg} />
-		</div>
-	);
-}
+  return (
+    <div
+      className="emoticon"
+      style={{ backgroundColor: colorsData[settings.color - 1].value }}
+    >
+      <img className="emoticon__eyes" src={eyesData[settings.eyes - 1].image} />
+      <img
+        className="emoticon__mouth"
+        src={mouthData[settings.mouth - 1].image}
+      />
+    </div>
+  );
+};
 
 export default Emoticon;
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import Emoticon from './components/Emoticon';
 import Configurator from './components/Configurator';
+import { SettingContext } from './setting-context.js';
 
 const App = () => {
+  const [settings, setSettings] = useState({
+    eyes: 2,
+    mouth: 3,
+    color: 6,
+  });
+
   return (
-    <div className="container">
-      <header>
-        <h1>Vytvoř si vlastní emotikon</h1>
-      </header>
-      <main>
-        <Emoticon />
-        <Configurator />
-      </main>
-    </div>
+    <SettingContext.Provider value={settings}>
+      <div className="container">
+        <header>
+          <h1>Vytvoř si vlastní emotikon</h1>
+        </header>
+        <main>
+          <Emoticon />
+          <Configurator />
+        </main>
+      </div>
+    </SettingContext.Provider>
   );
 };
 
-createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
